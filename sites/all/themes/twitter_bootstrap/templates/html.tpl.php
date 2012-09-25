cb0ba6d23fdf2247aba59e0903450af0bc5a2270
Quang Pham <me@quangpham.com>
2012-09-26 00:19:53 +0300

    Upgrade twitter bootstrap

--- sites/all/themes/twitter_bootstrap/templates/html.tpl.php
+++ sites/all/themes/twitter_bootstrap/templates/html.tpl.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php print $language->language; ?>">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js" type="text/javascript"></script>
   <?php print $scripts; ?>
   <!-- HTML5 element support for IE6-8 -->
   <!--[if lt IE 9]>
