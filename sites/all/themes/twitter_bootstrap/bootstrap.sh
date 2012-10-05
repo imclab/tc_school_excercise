4e48b00a1242b389b97fb919cce4a0b08f6c4ca4
Quang Pham <me@quangpham.com>
2012-10-05 22:56:45 +0300

    Use LESS

--- sites/all/themes/twitter_bootstrap/bootstrap.sh
+++ sites/all/themes/twitter_bootstrap/bootstrap.sh
@@ -1,4 +1,7 @@
 lessc --yui-compress bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.min.css
 lessc --yui-compress css/override.less > css/override.min.css
 
+lessc bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.css
+lessc css/override.less > css/override.css
+
 /Users/quang.pham/Dev/lib/drush/drush cc all
