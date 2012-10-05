032e4b52d97113e8b464a4e29c76dad26275112b
Quang Pham <me@quangpham.com>
2012-10-05 23:05:54 +0300

    Update less.sh

--- sites/all/themes/twitter_bootstrap/_less.sh
+++ sites/all/themes/twitter_bootstrap/_less.sh
@@ -1,7 +1,12 @@
+# Create min css file
 lessc --yui-compress bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.min.css
+lessc --yui-compress bootstrap/less/bootstrap-responsive.css.less > bootstrap/css/bootstrap-responsive.css.min.css
 lessc --yui-compress css/override.less > css/override.min.css
 
+# Create debug css file
 lessc bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.css
+lessc bootstrap/less/bootstrap-responsive.css.less > bootstrap/css/bootstrap-responsive.css.css
 lessc css/override.less > css/override.css
 
+# Clear all cache
 /Users/quang.pham/Dev/lib/drush/drush cc all
