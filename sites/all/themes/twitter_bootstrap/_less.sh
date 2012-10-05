4d219d1a352f8ff737feed5858ec4591bc5ff4db
Quang Pham <me@quangpham.com>
2012-10-05 23:19:00 +0300

    Modified _less.sh

--- sites/all/themes/twitter_bootstrap/_less.sh
+++ sites/all/themes/twitter_bootstrap/_less.sh
@@ -1,12 +1,12 @@
 # Create min css file
 lessc --yui-compress bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.min.css
 lessc --yui-compress bootstrap/less/responsive.less > bootstrap/css/bootstrap-responsive.min.css
-lessc --yui-compress css/override.less > css/override.min.css
+lessc --yui-compress css/less/override.less > css/override.min.css
 
 # Create debug css file
 lessc bootstrap/less/bootstrap.less > bootstrap/css/bootstrap.css
 lessc bootstrap/less/responsive.less > bootstrap/css/bootstrap-responsive.css
-lessc css/override.less > css/override.css
+lessc css/less/override.less > css/override.css
 
 # Clear all cache
 /Users/quang.pham/Dev/lib/drush/drush cc all
