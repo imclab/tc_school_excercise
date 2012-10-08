1a9cf9b951d93db2bee282be2fc639c2561f5a30
Quang Pham <me@quangpham.com>
2012-10-08 04:35:37 +0300

    Add logos

--- sites/all/themes/twitter_bootstrap/templates/page/page.tpl.php
+++ sites/all/themes/twitter_bootstrap/templates/page/page.tpl.php
@@ -5,44 +5,60 @@
 	//print render (file_create_url($node->field_splash_page_picture['und'][0]['uri'])); 
 	// print count($node->field_splash_page_picture);
 	// print count($node->field_splash_page_text);
-	// print '<pre>';
 	// //print_r($node->field_splash_page_picture['und']);
+	
+	// if (isset($node->field_splash_page_picture)) echo 'aYES';
+	// echo count($node->field_splash_page_picture);
+	// print '<pre>';
+	// 
 	// print_r($node);
 	// print '</pre>';
+	
 ?>
 
-
-<nav id="global-nav">
+<!-- NAVIGATION -->
+<nav id="top-nav">
 	<div class="container">
-		<a id="logo" href="/"><img src="<?php print '/'.path_to_theme().'/bootstrap/img/logo70x50.png';?>"></a>
-		
+		<a id="logo" href="/">
+			<img src="<?php print '/'.path_to_theme().'/bootstrap/img/logo70x50.png';?>">
+		</a>
 		<?php if ($primary_nav): ?>
 		  <?php print $primary_nav; ?>
 		<?php endif; ?>
 	</div>
 </nav>
+<!-- END OF NAVIGATION -->
 
-
-<?php if (isset($node->field_splash_page_picture)): ?>
+<!-- SPLASH -->
+<!-- If node contain splash image ?-->
+<?php if ((isset($node->field_splash_page_picture)) && (count($node->field_splash_page_picture))) : ?>
 	<section style="background-image: url(&quot;<?php echo render(file_create_url($node->field_splash_page_picture['und'][0]['uri']));?>&quot;); background-size: cover; height:<?php echo $node->field_splash_page_picture['und'][0]['height'];?>px;" class="background-cover" id="top-splash">
 		<div class="container">
 			<div class="centerbox">
-				<!-- <h1>About us</h1> -->
-				<h2><?php echo $node->field_splash_page_text['und'][0]['value'];?></h2>
+				<?php
+					if ($node->field_splash_page_heading) {
+						echo '<h1>'.$node->field_splash_page_heading['und'][0]['value'].'</h1>';
+					}
+					if ($node->field_splash_page_text) {
+						echo '<h2>'.$node->field_splash_page_text['und'][0]['value'].'</h2>';
+					}
+				?>
 			</div>
 		</div>
 	</section>
+	<div class="gradient-separator"></div>
+<!-- If node doesn't contain splash image-->	
 <?php else: ?>
 <section style="" class="background-cover" id="top-splash">
 	<div class="container">
 	</div>
 </section>
 <?php endif; ?>
-
+<!-- END OF SPLASH -->
 
 
 <div class="container">
-
+	
   <header role="banner" id="page-header">
     <?php if ( $site_slogan ): ?>
       <p class="lead"><?php print $site_slogan; ?></p>
